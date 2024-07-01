ca9426437edfac628956be4ee1ab999017aa6be2
yf-yeoo <yeo.yik.feng@gmail.com>
2024-07-01 16:40:30 +0800

    added in our addToCart feature

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,7 +1,11 @@
-import './product-card.styles.scss'
+import './product-card.styles.scss';
+import {CartContext} from '../../contexts/cart.context';
 import Button from '../button/button-component';
+import { useContext } from 'react';
 const ProductCard = ({ product }) => {
   const { name, price, imageUrl } = product;
+  const { addItemToCart } = useContext(CartContext);
+  const addProductToCart = () => addItemToCart(product);
   return (
     <div className='product-card-container'>
         <img src={imageUrl} alt={`${name}`} />
@@ -9,7 +13,7 @@ const ProductCard = ({ product }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
         </div>
-        <Button buttonType='inverted'>Add to cart</Button>
+        <Button buttonType='inverted' onClick={addProductToCart}>Add to cart</Button>
     </div>
   )
 }
